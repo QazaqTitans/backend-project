397688bc406466fd92085b31b708ee8f307af997
Yerden01 <mr.bulanbaev@gmail.com>
2021-05-09 22:56:53 +0600

    started creating order

---
+++ src/main/java/kz/reserve/backend/controller/OrderClientController.java
@@ -0,0 +1,32 @@
+package kz.reserve.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/client/order")
+public class OrderClientController {
+
+    @GetMapping()
+    public ResponseEntity<?> getMyOrders() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> makeOrder() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> updateOrder() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> cancelOrder() {
+        return null;
+    }
+}
