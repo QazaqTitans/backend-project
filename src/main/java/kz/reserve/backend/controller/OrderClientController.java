be39e21579d2f27aac00ffdefd20f36e8203ea09
Yerden01 <mr.bulanbaev@gmail.com>
2021-05-09 23:34:06 +0600

    fixed order bugs

--- src/main/java/kz/reserve/backend/controller/OrderClientController.java
+++ src/main/java/kz/reserve/backend/controller/OrderClientController.java
@@ -6,27 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/api/client/order")
+//@Controller
+//@RequestMapping("/api/client/order")
 public class OrderClientController {
 
-    @GetMapping()
-    public ResponseEntity<?> getMyOrders() {
-        return null;
-    }
-
-    @PostMapping
-    public ResponseEntity<?> makeOrder() {
-        return null;
-    }
-
-    @PostMapping
-    public ResponseEntity<?> updateOrder() {
-        return null;
-    }
-
-    @PostMapping
-    public ResponseEntity<?> cancelOrder() {
-        return null;
-    }
+//    @GetMapping()
+//    public ResponseEntity<?> getMyOrders() {
+//        return null;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<?> makeOrder() {
+//        return null;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<?> updateOrder() {
+//        return null;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<?> cancelOrder() {
+//        return null;
+//    }
 }
